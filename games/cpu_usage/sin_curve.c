505e910eb31ec34b91e84b81a265ed62fc7cf5ae
xyt <xuyangtong@gmail.com>
2014-05-12 17:15:42 +0800

    start sin

--- games/cpu_usage/sin_curve.c
+++ games/cpu_usage/sin_curve.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
-
-const int SLEEP_TIME 20000;
+#include<math.h>
 
 int main()
 {
+  const int sample_rate = 500;
+  
   while(true){
-    
+    for (i = 0; i < sample_rate; i++){
+      for (j = 0; j < run_time[i]; j++){
+      }
+      usleep(sleep_time[i]);
+    }
   }
   return 0;
 }
